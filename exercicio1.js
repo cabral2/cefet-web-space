85612434a8e95a3773e358a31d6d5d2839ebf76f
cabral2 <saviocampolina9@gmail.com>
2021-06-22 21:24:04 -0300

    Exercices 1, 2 and 3

--- exercicio1.js
+++ exercicio1.js
@@ -1,2 +1,18 @@
 // Faça o exercício da equação de GRAVITAÇÃO UNIVERSAL aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+let calculateButton = document.querySelector("#calcular");
+
+calculateButton.addEventListener("click", () => {
+  let m1Element = document.querySelector("#massa1");
+  let m2Element = document.querySelector("#massa2");
+  let dElement = document.querySelector("#distancia");
+  let gElement = document.querySelector("#constante");
+  let resultElement = document.querySelector("#resultado");
+
+  const m1 = m1Element && m1Element.value ? m1Element.value : 0;
+  const m2 = m2Element && m2Element.value ? m2Element.value : 0;
+  const d = dElement && dElement.value ? dElement.value : 1;
+  const g = gElement && gElement.value ? gElement.value : 0;
+
+  resultElement.value = (g * m1 * m2) / d ** 2;
+});
--- exercicio3.js
+++ exercicio3.js
@@ -1,2 +1,12 @@
 // Faça o exercício dos PARÁGRAFOS aqui
 // Este arquivo AINDA NÃO ESTÁ INCLUÍDO no arquivo HTML
+
+let expandButtons = document.querySelectorAll(".botao-expandir-retrair");
+
+expandButtons.forEach((button) => {
+  button.addEventListener("click", (ev) => {
+    const added = ev.currentTarget.parentElement.classList.toggle("expandido");
+    if (added) ev.currentTarget.innerHTML = "-";
+    else ev.currentTarget.innerHTML = "+";
+  });
+});
